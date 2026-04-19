3bcd49394aa31af5688cf977bd50ebe6b062a194
mrlbni <suchitamiki06@gmail.com>
2026-04-19 22:04:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-
+BOT_TOKEN = "8290068124:AAH_r4beyRhFUcid9ZXuybmaeruf4QIBLIw"
+OWNER_ID = 8565045255
+TELEGRAM_API = 22419004
+TELEGRAM_HASH = "34982b52c4a83c2af3ce8f4fe12fe4e1"
+DATABASE_URL = "mongodb+srv://mrnoobx:DAZCdTczVWyECi04@cluster0.sedgwxy.mongodb.net/?retryWrites=true&w=majority"
